6e3869ae90800b0159c58cd2eaeef9bb38e78a96
NickZ <nikolayzhmurov88@gmail.com>
2026-01-08 17:36:18 +0300

    Создана структура проекта

---
+++ main.py
@@ -0,0 +1,13 @@
+# main.py
+"""Entry point for ValutaTrade Hub."""
+import sys
+
+
+def main() -> None:
+    """Main function."""
+    print("ValutaTrade Hub запущен. Пока нет команд.")
+    sys.exit(0)
+
+
+if __name__ == "__main__":
+    main()
